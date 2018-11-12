6d84a237189abacbc6d32af3faec72c52eae8b89
Binaya Rimal <rimal.binaya@gmail.com>
2018-11-12 14:47:06 -0800

    figured out

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todos:[
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ],
+      newDescription: ""
+    }
+  }
+
+  toggleComplete(position) {
+
+    const todos2 = this.state.todos.slice();
+    const todo = todos2[position];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos2 });
+
+   }
+
+
+  handleChange(e) {
+    this.setState({ newDescription: e.target.value })
+  }
+  handleSubmit(e) {
+   e.preventDefault();
+   if (!this.state.newDescription) { return }
+   const newTodo = { description: this.state.newDescription, isCompleted: false };
+
+   this.setState({ todos: [...this.state.todos, newTodo], newDescription: '' });
+ }
+
+
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ul>
+       {this.state.todos.map((todo,index) =>
+            <ToDo key={ index }
+            description={ todo.description }
+            isCompleted={ todo.isCompleted }
+            toggleComplete={ () => this.toggleComplete(index) }
+
+
+          />
+)}
+     </ul>
+     <form   onSubmit={ (e) => this.handleSubmit(e)}>
+          <input
+          type="text"
+          value ={this.state.newDescription}
+          onChange={ (e) => this.handleChange(e) }/>
+
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
